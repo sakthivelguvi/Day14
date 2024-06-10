b737cb2604649454b8b75bb4ea105068a612ffc0
sakthivelguvi <sakthivel.mando@gmail.com>
2024-06-10 06:06:28 +0530

    Class work

---
+++ script.js
@@ -0,0 +1,18 @@
+    // console.log(window.location);
+
+    // function sayHello() {
+
+    //     console.log('Hello Guest!');
+    // }
+    // window.setTimeout(sayHello,5000);
+
+    // let sayHello= ()=> {
+
+    //     console.log('Hello Guest!');
+    // }
+    // window.setTimeout(sayHello,5000);
+
+    window.setTimeout( ()=>{
+        console.log('Hello Guest!');
+
+    },5000)
