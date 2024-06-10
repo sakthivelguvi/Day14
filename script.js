1f27a1038cd78247f262ef8a583866b67922d744
sakthivelguvi <sakthivel.mando@gmail.com>
2024-06-10 06:55:23 +0530

    added function for time interval

--- script.js
+++ script.js
@@ -12,7 +12,17 @@
     // }
     // window.setTimeout(sayHello,5000);
 
-    window.setTimeout( ()=>{
-        console.log('Hello Guest!');
+    // window.setTimeout( ()=>{
+    //     console.log('Hello Guest!');
 
-    },5000)
+    // },5000)
+
+    // setTimeout (()=>{
+    //     document.body.style.backgroundColor='Yellow';
+
+    // }, 3000);
+    // let downloadButton =document.getElementById('download');
+    // setTimeout( ()=>{
+    //     downloadButton.removeAttribute('disabled');
+    // },3000);
+    
